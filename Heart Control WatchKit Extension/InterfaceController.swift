b273faa46dd218b5b2f37594e16d38112a82514c
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-08-01 09:38:48 +0200

    Initial commit

---
+++ Heart Control WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Heart Control WatchKit Extension
+//
+//  Created by Thomas Paul Mann on 01/08/16.
+//  Copyright © 2016 Thomas Paul Mann. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: AnyObject?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
