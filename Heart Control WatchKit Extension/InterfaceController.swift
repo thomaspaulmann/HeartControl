1b973eaf827d3216e884212c2321df0025803af8
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-08-01 09:58:29 +0200

    Start simple Workout Session

--- Heart Control WatchKit Extension/InterfaceController.swift
+++ Heart Control WatchKit Extension/InterfaceController.swift
@@ -8,24 +8,67 @@
 
 import WatchKit
 import Foundation
-
+import HealthKit
 
 class InterfaceController: WKInterfaceController {
 
+    // MARK: - Properties
+
+    private let healthStore = HKHealthStore()
+    private var workoutStartDate: Date?
+
+    // MARK: - Lifecycle
+
     override func awake(withContext context: AnyObject?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
+
     }
     
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+
+        // Workout Configuration
+
+        let workoutConfiguration = HKWorkoutConfiguration()
+        workoutConfiguration.activityType = .other
+        workoutConfiguration.locationType = .indoor
+
+        // Start Workout
+
+        do {
+            let workoutSession = try HKWorkoutSession(configuration: workoutConfiguration)
+
+            workoutSession.delegate = self
+
+            healthStore.start(workoutSession)
+        } catch {
+            // ...
+        }
+
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+
+}
+
+// MARK: - Workout Session Delegate
+
+extension InterfaceController: HKWorkoutSessionDelegate {
+
+    func workoutSession(_ workoutSession: HKWorkoutSession, didChangeTo toState: HKWorkoutSessionState, from fromState: HKWorkoutSessionState, date: Date) {
+
+    }
+
+
+    func workoutSession(_ workoutSession: HKWorkoutSession, didFailWithError error: NSError) {
+
+    }
+
+    func workoutSession(_ workoutSession: HKWorkoutSession, didGenerate event: HKWorkoutEvent) {
+
     }
 
 }
