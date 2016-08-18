b2d361bc9e05066be938533bdd34aa91f4b8bd4e
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-08-18 11:49:48 +0200

    Fix call of completion handler for authorization

--- Heart Control WatchKit Extension/AuthorizationManger.swift
+++ Heart Control WatchKit Extension/AuthorizationManger.swift
@@ -11,22 +11,20 @@ import HealthKit
 class AuthorizationManager {
 
     static func requestAuthorization(completionHandler: ((success: Bool) -> Void)) {
-        defer {
-            completionHandler(success: false)
-        }
-
         // Create health store.
         let healthStore = HKHealthStore()
 
         // Check if there is health data available.
         if (!HKHealthStore.isHealthDataAvailable()) {
             print("No health data is available.")
+            completionHandler(success: false)
             return
         }
 
         // Create quantity type for heart rate.
         guard let heartRateQuantityType = HKQuantityType.quantityType(forIdentifier: .heartRate) else {
             print("Unable to create quantity type for heart rate.")
+            completionHandler(success: false)
             return
         }
 
@@ -35,6 +33,7 @@ class AuthorizationManager {
             // If there is an error, do nothing.
             guard error == nil else {
                 print(error)
+                completionHandler(success: false)
                 return
             }
 
